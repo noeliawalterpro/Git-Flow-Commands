a108e9180318e9c4df1b3e216eb976667b063c2e
noeliawalterpro <noeliawalterpro@gmail.com>
2023-11-08 12:06:15 -0500

    release from develop

--- start-release.sh
+++ start-release.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Prompt the user for the release version
+
 read -p "Enter the release version (e.g., 1.0.0): " RELEASE_VERSION
 
+# Create a new release branch
+git checkout develop
 # Create a new release branch
 git checkout -b release/$RELEASE_VERSION
